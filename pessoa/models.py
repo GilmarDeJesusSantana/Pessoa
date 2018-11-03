2c0619f1ed1ed737c26e02243331ee589a20daa8
Gilmar de Jesus Santana <gilmar.jesus@gmail.com>
2018-11-03 12:43:17 -0300

    Unica tabela de pessoas.

--- pessoa/models.py
+++ pessoa/models.py
@@ -2,19 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Endereco(models.Model):
-    rua = models.CharField(max_length=100)
-    numero = models.CharField(max_length=10)
-    bairro = models.CharField(max_length=50)
-    cep = models. CharField(max_length=8)
-    bloco = models.CharField(max_length=3)
-    apartamento = models.CharField(max_length=4)
-    complemento = models.CharField(max_length= 20)
-
 class  Pessoa(models.Model):
     nome = models.CharField(max_length=100)
     titulo= models.CharField(max_length=12)
     cpf = models.CharField(max_length=11)
     telefone = models.CharField(max_length=15)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
     flag_delete = models.CharField(max_length=1)
+    #Endereco
+    rua = models.CharField(max_length=100)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=50)
+    cep = models.CharField(max_length=8)
+    bloco = models.CharField(max_length=3)
+    apartamento = models.CharField(max_length=4)
+    complemento = models.CharField(max_length=20)
--- pessoa/views.py
+++ pessoa/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.views.generic.base import TemplateView
-from pessoa.models import Pessoa, Endereco
+from pessoa.models import Pessoa
 from django import forms
 
 def index(request):
@@ -17,20 +17,19 @@ class CadastroPessoaView(TemplateView):
 
     def post(self, request):
         dados_form = forms.data
-        endereco = Endereco(rua=dados_form['rua'],
-                            numero=dados_form['numero'],
-                            bairro=dados_form['bairro'],
-                            cep=dados_form['cep'],
-                            bloco=dados_form['bloco'],
-                            apartamento=dados_form['apartamento'],
-                            complemento=dados_form['complemento'])
-        Endereco.save()
 
         pessoa = Pessoa(nome=dados_form['nome'],
                         titulo= dados_form['titulo'],
                         cpf=dados_form['cpf'],
                         telefone= dados_form['telefone'],
                         flag_delete='A',
+                        rua=dados_form['rua'],
+                        numero=dados_form['numero'],
+                        bairro=dados_form['bairro'],
+                        cep=dados_form['cep'],
+                        bloco=dados_form['bloco'],
+                        apartamento=dados_form['apartamento'],
+                        complemento=dados_form['complemento']
         )
         Pessoa.save()
         print ('Dados Registrados!!')
