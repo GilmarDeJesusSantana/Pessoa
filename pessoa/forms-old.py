c8cebd08e223c49764a044aef27bf40c1e36b810
Gilmar de Jesus Santana <gjsantana@scopus.com.br>
2018-12-27 15:52:43 -0200

    Ajustes no Layout.

--- pessoa/forms-old.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Pessoa
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Pessoa
-        fields = ('nome','titulo','cpf','telefone','cep','rua','numero','bairro','apartamento','bloco','complemento',)
