94af290e1041d79281fd2d9f5441234ea5f129c7
Gilmar de Jesus Santana <gjsantana@scopus.com.br>
2019-01-16 13:29:32 -0200

    Centralizacao das classes que chamam as view. Inicio da implementacao da tela de detalhe.

---
+++ pessoa/people.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from pessoa.search import SearchPeople
+from pessoa.forms import RegistraPessoaForm
+from .models import Pessoa
+from django.db.models import Q
+from django.views.generic.base import TemplateView
+
+class PeopleDetails(TemplateView):
+    template_details = 'details.html'
+
+    def get(self,request):
+        return render(request,self.template_details)
+
+class PeopleSearch(TemplateView):
+    template_search = 'pessoa_search.html'
+    def get(self,request):
+        pessoas = Pessoa.objects.all()
+        return render(request,self.template_search,{'pessoas':pessoas})
+    def post(self,request):
+        print('Classe pessoaSearch --  metodo POST')
+        form_search = SearchPeople(request.POST)
+        dados_search = form_search.data
+        people_search = Pessoa.objects.filter(Q(nome__startswith=dados_search['nome']))
+        return render(request, self.template_search, {'pessoas': people_search})
+
+class CadastroPessoaView(TemplateView):
+    template_name = 'pessoaForm.html'
+    # este metodo envia o html para o browser
+    def get(self, request):
+        print('Metodo chamado----> GET.')
+        return render(request, self.template_name)
+        print('Render processado-->')
+
+    def post(self, request):
+        """A Classe chamada abaixo Valida o formulario"""
+        form = RegistraPessoaForm(request.POST)
+        dados_form = form.data
+
+        pessoa = Pessoa(nome=dados_form['nome'],
+                        titulo= dados_form['titulo'],
+                        cpf=dados_form['cpf'],
+                        telefone= dados_form['telefone'],
+                        flag_delete='A',
+                        rua=dados_form['rua'],
+                        numero=dados_form['numero'],
+                        bairro=dados_form['bairro'],
+                        cep=dados_form['cep'],
+                        bloco=dados_form['bloco'],
+                        apartamento=dados_form['apartamento'],
+                        complemento=dados_form['complemento']
+        )
+        pessoa.save()
+        print ('Dados Registrados!!')
+        return render(request,'pessoaForm.html')
--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from pessoa.views import index, CadastroPessoaView, pessoaSearch
+from pessoa.views import index, CadastroPessoaView, PeopleSearch, PeopleDetails
 urlpatterns = [
    url(r'^$',index,name = 'index'),
    url(r'^cadastro_pessoa/$',CadastroPessoaView.as_view(), name='cadastro_pessoa'),
-   url(r'^pessoa_search/$',pessoaSearch.as_view(),name='pessoa_search'),
+   url(r'^pessoa_search/$',PeopleSearch.as_view(),name='pessoa_search'),
+   url(r'^details/$',PeopleDetails.as_view(),name='details'),
 ]
