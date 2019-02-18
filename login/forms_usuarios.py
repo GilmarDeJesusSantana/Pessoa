77358d21ce1cb15e69cb3051992cdf8254fce2a1
Gilmar de Jesus Santana <gjsantana@scopus.com.br>
2019-02-18 16:19:48 -0300

    Login

--- login/forms_usuarios.py
+++ login/forms_usuarios.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from login.models import Usuario
+
 class UsuarioForm(forms.Form):
     nome_user = forms.CharField(required=True)
     usuario = forms.CharField(required=True)
@@ -13,11 +14,23 @@ class UsuarioForm(forms.Form):
             self.adiciona_erro('Verifique os dados informados.')
             valid = False
         user_exists = Usuario.objects.filter(nome_user__contains=self.data['nome_user'])
-
         if user_exists:
             self.adiciona_erro ('Usuário já cadastrado.')
             valid = False
         return valid
+
+    def login_is_valid(self):
+        valid = True
+        usuario= self.data['nome_user']
+        senha= self.data['senha']
+        user_login = Usuario.objects.get(nome_user=usuario)
+
+        if user_login.nome_user == usuario && user_login.senha == senha:
+            return render
+
+
+        print('Usuario =======>>', user_login.senha, 'Para ser logado')
+        print ('Usuario ======> ', user_login_exists)
 
     def adiciona_erro(self,message):
         errors = self._errors.setdefault(forms.forms.NON_FIELD_ERRORS, forms.utils.ErrorList())
--- login/login.py
+++ login/login.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from pessoa.views import index
-
+from login.forms_usuarios import UsuarioForm
 class Login(TemplateView):
     template_login = 'login.html'
 
@@ -9,4 +9,6 @@ class Login(TemplateView):
         return render(request,self.template_login)
 
     def post(self,request):
-        return index(request)
+        form = UsuarioForm(request.POST)
+        if form.login_is_valid():
+            return index(request)
